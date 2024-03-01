5fe398aecb25cb9fb992213b776114cfde61bac5
jonny <jonny.alves@zup.com.br>
2024-03-01 10:20:00 -0300

    atualizacao de topico e codigos http

--- src/main/kotlin/br/com/forum/controller/TopicoController.kt
+++ src/main/kotlin/br/com/forum/controller/TopicoController.kt
@@ -1,16 +1,23 @@
 package br.com.forum.controller
 
+import br.com.forum.dto.AtualizarTopicoForm
 import br.com.forum.dto.NovoTopicoForm
 import br.com.forum.dto.TopicoView
 import br.com.forum.service.TopicoService
 import jakarta.validation.Valid
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.DeleteMapping
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.PutMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.ResponseStatus
 import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.util.UriComponentsBuilder
 
 @RestController
 @RequestMapping("/topicos")
@@ -28,8 +35,24 @@ class TopicoController(
     }
 
     @PostMapping
-    fun cadastrar(@RequestBody @Valid novoTopicoForm: NovoTopicoForm
-    ) {
-        topicoService.cadastrar(novoTopicoForm)
+    fun cadastrar(
+        @RequestBody @Valid novoTopicoForm: NovoTopicoForm,
+        uriBuilder: UriComponentsBuilder
+    ) :ResponseEntity<TopicoView>{
+        val topicoCriado = topicoService.cadastrar(novoTopicoForm)
+        val uri = uriBuilder.path("/topicos/{$topicoCriado.id}").build().toUri()
+        return ResponseEntity.created(uri).body(topicoCriado)
+    }
+
+    @PutMapping()
+    fun atualizar(@RequestBody @Valid atualizarTopico: AtualizarTopicoForm):ResponseEntity<TopicoView>{
+        val topicoAtualizado = topicoService.atualizar(atualizarTopico)
+        return ResponseEntity.ok(topicoAtualizado)
+    }
+
+    @DeleteMapping("/id")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    fun deletar(@PathVariable id: Long){
+        topicoService.deletar(id)
     }
 }
--- src/main/kotlin/br/com/forum/service/TopicoService.kt
+++ src/main/kotlin/br/com/forum/service/TopicoService.kt
@@ -1,5 +1,6 @@
 package br.com.forum.service
 
+import br.com.forum.dto.AtualizarTopicoForm
 import br.com.forum.dto.NovoTopicoForm
 import br.com.forum.dto.TopicoView
 import br.com.forum.mapper.TopicoFormMapper
@@ -12,11 +13,13 @@ import java.util.*
 import java.util.stream.Collectors
 
 @Service
-class TopicoService(private var topicos: List<Topico>,
+class TopicoService(
+    private var topicos: List<Topico>,
     private val cursoService: CursoService,
     private val usuarioService: UsuarioService,
     private val topicoViewMapper: TopicoViewMapper,
-    private val topicoFormMapper: TopicoFormMapper) {
+    private val topicoFormMapper: TopicoFormMapper
+) {
     init {
         val topico = Topico(
             id = 1,
@@ -63,7 +66,8 @@ class TopicoService(private var topicos: List<Topico>,
                 email = "ana@email.com"
             )
         )
-        topicos = Arrays.asList(topico, topico2, topico3
+        topicos = Arrays.asList(
+            topico, topico2, topico3
         )
     }
 
@@ -76,10 +80,33 @@ class TopicoService(private var topicos: List<Topico>,
         return topicoViewMapper.map(topico)
     }
 
-    fun cadastrar(novoTopicoForm: NovoTopicoForm) {
+    fun cadastrar(novoTopicoForm: NovoTopicoForm): TopicoView {
         val topico = topicoFormMapper.map(novoTopicoForm)
         topico.id = topicos.size.toLong() + 1
         topicos = topicos.plus(topico)
+        return topicoViewMapper.map(topico)
 
+    }
+
+    fun atualizar(atualizarTopico: AtualizarTopicoForm) :TopicoView{
+        val topico = topicos.stream().filter { topico -> topico.id == atualizarTopico.id }.findFirst().get()
+        val topicoAtualizado = Topico(
+            id = atualizarTopico.id,
+            titulo = atualizarTopico.titulo,
+            mensagem = atualizarTopico.mensagem,
+            autor = topico.autor,
+            curso = topico.curso,
+            respostas = topico.respostas,
+            status = topico.status,
+            dataCriacao = topico.dataCriacao
+        )
+
+        topicos = topicos.minus(topico).plus(topicoAtualizado)
+        return topicoViewMapper.map(topicoAtualizado)
+    }
+
+    fun deletar(id: Long) {
+        val topico = topicos.stream().filter { topico -> topico.id == id }.findFirst().get()
+        topicos = topicos.minus(topico)
     }
 }
